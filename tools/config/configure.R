57e3252c700883344256f384ec59e170ad97c7a6
Daniel Falbel <dfalbel@gmail.com>
2021-02-02 10:33:59 -0300

    try to use a configure file to disable compilation in solaris

---
+++ tools/config/configure.R
@@ -0,0 +1,16 @@
+# Prepare your package for installation here.
+# Use 'define()' to define configuration variables.
+# Use 'configure_file()' to substitute configuration values.
+
+configure_file("src/Makevars.in")
+
+is_solaris <- function() {
+  grepl('SunOS',Sys.info()['sysname'])
+}
+
+if (is_solaris()) {
+  define(SRC = "")
+} else {
+  define(SRC = "benchmark.cpp bitbase.cpp bitboard.cpp endgame.cpp evaluate.cpp main.cpp material.cpp misc.cpp movegen.cpp movepick.cpp pawns.cpp position.cpp psqt.cpp search.cpp tbprobe.cpp thread.cpp timeman.cpp tt.cpp uci.cpp ucioption.cpp")s
+}
+

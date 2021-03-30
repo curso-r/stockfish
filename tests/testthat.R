7ae8bf1053d41c2b12d79151f419fdf89ba1e6b4
C. Lente <7017340+clente@users.noreply.github.com>
2021-03-30 15:11:37 -0300

    Tidy style

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,12 +2,13 @@ library(testthat)
 library(stockfish)
 
 is_solaris <- function() {
-  grepl('SunOS', Sys.info()['sysname'])
+  grepl("SunOS", Sys.info()["sysname"])
 }
 
 is_m1 <- function() {
   !R.version$arch == "x86_64"
 }
 
-if (!is_solaris() && !is_m1())
+if (!is_solaris() && !is_m1()) {
   test_check("stockfish")
+}
